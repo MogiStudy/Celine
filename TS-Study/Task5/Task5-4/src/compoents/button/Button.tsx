7cd294098cbe6d8a1b384a4eee8272c45a867433
seonyeong719 <sy904280@gmail.com>
2023-10-26 23:40:08 +0900

    :sparkles: Feat: TS-Study

---
+++ TS-Study/Task5/Task5-4/src/compoents/button/Button.tsx
@@ -0,0 +1,27 @@
+export type ButtonStyle = {
+  variant: string;
+  size: string;
+  children: React.ReactNode;
+  onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void;
+};
+
+const Button: React.FC<ButtonStyle> = ({ variant, size, children, onClick, ...rest }) => {
+  return (
+    <button
+      style={{
+        backgroundColor: variant === "primary" ? "blue" : "red",
+        fontSize: size === "small" ? "10px" : "20px",
+        color: "white",
+        padding: "10px",
+        borderRadius: "5px",
+        border: "none",
+        cursor: "pointer",
+      }}
+      {...rest}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
---
+++ TS-Study/Task5/Task5-4/src/pages/main/index.tsx
@@ -0,0 +1,20 @@
+import { useNavigate } from "react-router-dom";
+import Button from "../../compoents/button/Button";
+
+const Home = () => {
+  const navigator = useNavigate();
+  const natvigateToTodoPage = () => {
+    navigator("/todo");
+    console.log("ddd");
+  };
+
+  return (
+    <div>
+      <h1>Home</h1>
+      <Button variant="primary" size="small" onClick={natvigateToTodoPage}>
+        Go to TodoPage
+      </Button>
+    </div>
+  );
+};
+export default Home;
